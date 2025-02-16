52ca9f7f2e42e77f1917b75f8fabe2c6e63aaee0
John Doe <john.doe@example.com>
2025-02-16 20:59:11 +0100

    react components structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
-import { Box, BoxWithChildren } from './types';
+import { Box, BoxWithChildren, BoxWithData } from './types';
 import { useData } from './hooks/data';
+import styled from 'styled-components'
 
 interface BoxTreeProps {
   boxes: Box[];
@@ -14,7 +15,13 @@ const BoxTree: React.FC<BoxTreeProps> = ({ boxes, depth = 0 }) => {
         <li key={index}>
           <span>
             {box.type} ({box.size} bytes, offset: {box.offset})
+
+            {"data" in box && (box as BoxWithData).data.byteLength > 0 && (
+              <DataContainer>data - {box.data.byteLength}</DataContainer>
+            )}
+
           </span>
+
 
           {"children" in box && (box as BoxWithChildren).children.length > 0 && (
             <BoxTree boxes={(box as BoxWithChildren).children} depth={depth + 1} />
@@ -38,3 +45,14 @@ function App() {
 }
 
 export default App;
+
+const DataContainer = styled.div`
+  background-color: #f0f0f0;
+  padding: 5px;
+  border-radius: 5px;
+  margin-top: 5px;
+  margin-left: 10px;
+  margin-right: 10px;
+  font-size: 0.8em;
+  font-family: monospace;
+`
