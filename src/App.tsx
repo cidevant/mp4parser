850539aa2eb8dbbd670f32e57237c39465441240
John Doe <john.doe@example.com>
2025-02-16 17:18:48 +0100

    parse

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
 import React, { useEffect, useState } from 'react';
 
+function parseData(data: ArrayBuffer) {
+  const view = new DataView(data);
+  const length = view.byteLength;
+  const result = new Uint8Array(length);
+
+  for (let i = 0; i < length; i++) {
+    result[i] = view.getUint8(i);
+  }
+  return result;
+  
+}
+
 function App() {
 	const [data, setData] = useState<ArrayBuffer>();
 	const [error, setError] = useState<string>("");
@@ -18,6 +30,12 @@ function App() {
         setError(err.message);
       });
 	}, []);
+
+  useEffect(() => {
+    if (data) {
+      console.log(parseData(data));
+    }
+  }, [data]);
   
 
 	return (
