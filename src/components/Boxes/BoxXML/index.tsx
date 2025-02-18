831e0427a8ab455be61935fa92286e37f8561347
John Doe <john.doe@example.com>
2025-02-18 18:31:14 +0100

    chore: small styling

--- src/components/Boxes/BoxXML/index.tsx
+++ src/components/Boxes/BoxXML/index.tsx
@@ -26,9 +26,7 @@ const BoxXML: React.FC<BoxWithXMLDataProps> = ({ box, depth = 0 }) => {
         <BoxTypeSpan type={box.type}>{box.type.toUpperCase()}</BoxTypeSpan> (length: {box.size} bytes, offset: {box.offset})
       </span>
       {box.xml && (
-        <div style={{ marginLeft: 20 }}>
-          <pre>{box.xml}</pre>
-        </div>
+        <pre>{box.xml}</pre>
       )}
       {images && images.length > 0 && (
         <>
